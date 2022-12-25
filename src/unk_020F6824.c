150912ba1b51160c44f61a8a0ec774831e66fa32
Nomura <72407407+Nomura-RH@users.noreply.github.com>
2022-12-25 09:53:28 +0100

    Match unk_020F6824.c

---
+++ src/unk_020F6824.c
@@ -0,0 +1,22 @@
+#include <nitro.h>
+#include <string.h>
+
+#include "struct_decls/struct_020067E8_decl.h"
+
+#include "constdata/const_020F6824.h"
+
+#include "struct_defs/struct_0208BE5C.h"
+
+FS_EXTERN_OVERLAY(overlay73);
+
+int ov73_021D0D80(UnkStruct_020067E8 * param0, int * param1);
+int ov73_021D0E20(UnkStruct_020067E8 * param0, int * param1);
+int ov73_021D0F7C(UnkStruct_020067E8 * param0, int * param1);
+
+
+const UnkStruct_0208BE5C Unk_020F6824 = {
+    ov73_021D0D80,
+    ov73_021D0E20,
+    ov73_021D0F7C,
+    FS_OVERLAY_ID(overlay73),
+};
