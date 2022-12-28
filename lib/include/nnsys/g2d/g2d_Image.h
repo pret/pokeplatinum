507a4d7b6be4be2828dd2ef3fed5089924c586b9
Nomura <72407407+Nomura-RH@users.noreply.github.com>
2022-12-28 18:34:40 +0100

    Match unk_0200A328.c

--- lib/include/nnsys/g2d/g2d_Image.h
+++ lib/include/nnsys/g2d/g2d_Image.h
@@ -1,5 +1,14 @@
-#ifndef NNSYS_G2D_G2D_IMAGE_H_
-#define NNSYS_G2D_G2D_IMAGE_H_
+#ifndef NNS_G2D_IMAGE_H_
+#define NNS_G2D_IMAGE_H_
+
+#include <nitro.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NNS_G2dInitializeImageProxy         NNS_G2dInitImageProxy
+#define NNS_G2dInitializeImagePaletteProxy  NNS_G2dInitImagePaletteProxy
 
 typedef enum NNS_G2D_VRAM_TYPE {
     NNS_G2D_VRAM_TYPE_3DMAIN = 0,
@@ -8,9 +17,14 @@ typedef enum NNS_G2D_VRAM_TYPE {
     NNS_G2D_VRAM_TYPE_MAX    = 3
 } NNS_G2D_VRAM_TYPE;
 
-typedef struct NNSG2dVRamLocation {
-    u32 baseAddrOfVram[ NNS_G2D_VRAM_TYPE_MAX ];
-} NNSG2dVRamLocation;
+#define NNS_G2D_VRAM_TYPE_VALID(val)                   \
+    NNS_G2D_ASSERTMSG((val) == NNS_G2D_VRAM_TYPE_3DMAIN ||  \
+                      (val) == NNS_G2D_VRAM_TYPE_2DMAIN ||  \
+                      (val) == NNS_G2D_VRAM_TYPE_2DSUB,   \
+                      "Invalid NNS_G2D_VRAM_TYPE type")   \
+
+#define NNS_G2D_VRAM_ADDR_NOT_INITIALIZED       0xFFFFFFFF
+#define NNS_G2D_VRAM_ADDR_NONE                  NNS_G2D_VRAM_ADDR_NOT_INITIALIZED
 
 typedef struct NNSG2dImageAttr {
     GXTexSizeS sizeS;
@@ -20,6 +34,10 @@ typedef struct NNSG2dImageAttr {
     GXTexPlttColor0 plttUse;
     GXOBJVRamModeChar mappingType;
 } NNSG2dImageAttr;
+
+typedef struct NNSG2dVRamLocation {
+    u32 baseAddrOfVram[NNS_G2D_VRAM_TYPE_MAX];
+} NNSG2dVRamLocation;
 
 typedef struct NNSG2dImageProxy {
     NNSG2dVRamLocation vramLocation;
@@ -32,11 +50,110 @@ typedef struct NNSG2dImagePaletteProxy {
     NNSG2dVRamLocation vramLocation;
 } NNSG2dImagePaletteProxy;
 
-void NNS_G2dLoadPaletteEx(const NNSG2dPaletteData* pSrcData, const NNSG2dPaletteCompressInfo* pCmpInfo, u32 addr, NNS_G2D_VRAM_TYPE type, NNSG2dImagePaletteProxy* pPltProxy);
-void NNS_G2dLoadPalette(const NNSG2dPaletteData* pSrcData, u32 addr, NNS_G2D_VRAM_TYPE type, NNSG2dImagePaletteProxy* pPltProxy);
-void NNS_G2dLoadImage1DMapping(const NNSG2dCharacterData* pSrcData, u32 baseAddr, NNS_G2D_VRAM_TYPE type, NNSG2dImageProxy* pImgProxy);
-void NNS_G2dLoadImage2DMapping(const NNSG2dCharacterData* pSrcData, u32 baseAddr, NNS_G2D_VRAM_TYPE type, NNSG2dImageProxy* pImgProxy);
-u32 NNS_G2dGetImageLocation (const NNSG2dImageProxy* pImg, NNS_G2D_VRAM_TYPE type);
-u32 NNS_G2dGetImagePaletteLocation(const NNSG2dImagePaletteProxy* pImg, NNS_G2D_VRAM_TYPE type);
+void NNS_G2dInitImageProxy(NNSG2dImageProxy * pImg);
+void NNS_G2dSetImageLocation(NNSG2dImageProxy * pImg, NNS_G2D_VRAM_TYPE type, u32 addr);
+u32  NNS_G2dGetImageLocation(const NNSG2dImageProxy * pImg, NNS_G2D_VRAM_TYPE type);
+BOOL NNS_G2dIsImageReadyToUse(const NNSG2dImageProxy * pImg, NNS_G2D_VRAM_TYPE type);
+void NNS_G2dLoadImage1DMapping
+(
+    const NNSG2dCharacterData * pSrcData,
+    u32 baseAddr,
+    NNS_G2D_VRAM_TYPE type,
+    NNSG2dImageProxy * pImgProxy
+);
+void NNS_G2dLoadImage2DMapping
+(
+    const NNSG2dCharacterData * pSrcData,
+    u32 baseAddr,
+    NNS_G2D_VRAM_TYPE type,
+    NNSG2dImageProxy * pImgProxy
+);
+void NNS_G2dLoadImageVramTransfer
+(
+    const NNSG2dCharacterData * pSrcData,
+    u32 baseAddr,
+    NNS_G2D_VRAM_TYPE type,
+    NNSG2dImageProxy * pImgProxy
+);
+void NNS_G2dLoadPalette
+(
+    const NNSG2dPaletteData * pSrcData,
+    u32 addr,
+    NNS_G2D_VRAM_TYPE type,
+    NNSG2dImagePaletteProxy * pPltProxy
+);
+void NNS_G2dLoadPaletteEx
+(
+    const NNSG2dPaletteData * pSrcData,
+    const NNSG2dPaletteCompressInfo * pCmpInfo,
+    u32 addr,
+    NNS_G2D_VRAM_TYPE type,
+    NNSG2dImagePaletteProxy * pPltProxy
+);
 
-#endif //NNSYS_G2D_G2D_IMAGE_H_
+void NNS_G2dInitImagePaletteProxy(NNSG2dImagePaletteProxy * pImg);
+void NNS_G2dSetImagePaletteLocation(NNSG2dImagePaletteProxy * pImg, NNS_G2D_VRAM_TYPE type, u32 addr);
+u32 NNS_G2dGetImagePaletteLocation(const NNSG2dImagePaletteProxy * pImg, NNS_G2D_VRAM_TYPE type);
+BOOL NNS_G2dIsImagePaletteReadyToUse(const NNSG2dImagePaletteProxy * pImg, NNS_G2D_VRAM_TYPE type);
+static BOOL NNS_G2dIsPaletteImageFmt(const NNSG2dImageAttr * imgAttr);
+void NNSi_G2dInitializeVRamLocation(NNSG2dVRamLocation * pVramLocation);
+void NNSi_G2dSetVramLocation(NNSG2dVRamLocation * pVramLocation, NNS_G2D_VRAM_TYPE type, u32 addr);
+u32  NNSi_G2dGetVramLocation(const NNSG2dVRamLocation * pVramLocation, NNS_G2D_VRAM_TYPE type);
+BOOL NNSi_G2dIsVramLocationReadyToUse(const NNSG2dVRamLocation * pVramLocation, NNS_G2D_VRAM_TYPE type);
+
+void NNSi_G2dDoImageLoadingToVram
+(
+    const NNSG2dCharacterData * pSrcData,
+    u32 baseAddr,
+    NNS_G2D_VRAM_TYPE type
+);
+void NNSi_G2dSetupImageProxyPrams
+(
+    const NNSG2dCharacterData * pSrcData,
+    u32 baseAddr,
+    NNS_G2D_VRAM_TYPE type,
+    NNSG2dImageProxy * pImgProxy
+);
+
+void NNSi_G2dDoLoadingPaletteToVram
+(
+    const NNSG2dPaletteData * pSrcData,
+    u32 addr,
+    NNS_G2D_VRAM_TYPE type
+);
+
+void NNSi_G2dDoLoadingPaletteToVramEx
+(
+    const NNSG2dPaletteData * pSrcData,
+    const NNSG2dPaletteCompressInfo * pCmpInfo,
+    u32 addr,
+    NNS_G2D_VRAM_TYPE type
+);
+
+void NNSi_G2dSetupPaletteProxyPrams
+(
+    const NNSG2dPaletteData * pSrcData,
+    u32 addr,
+    NNS_G2D_VRAM_TYPE type,
+    NNSG2dImagePaletteProxy * pPltProxy
+);
+
+inline BOOL NNS_G2dIsPaletteImageFmt (const NNSG2dImageAttr * imgAttr)
+{
+    NNS_G2D_NULL_ASSERT(imgAttr);
+    return (imgAttr->fmt == GX_TEXFMT_PLTT4 ||
+            imgAttr->fmt == GX_TEXFMT_PLTT16 ||
+            imgAttr->fmt == GX_TEXFMT_PLTT256) ? TRUE : FALSE;
+}
+
+inline void NNS_G2dSetImageExtPaletteFlag (NNSG2dImageProxy * pImgProxy, BOOL bUseExtPlt)
+{
+    NNS_G2D_NULL_ASSERT(pImgProxy);
+    pImgProxy->attr.bExtendedPlt = bUseExtPlt;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/unk_0200A328.c
@@ -0,0 +1,325 @@
+#include <nitro.h>
+#include <string.h>
+
+#include "struct_decls/struct_02009DC8_decl.h"
+
+#include "struct_defs/struct_02009CFC.h"
+#include "struct_defs/struct_0200A328.h"
+#include "struct_defs/struct_0200A5C8.h"
+
+#include "unk_02009714.h"
+#include "unk_0200A328.h"
+#include "unk_0201E86C.h"
+#include "unk_0201F834.h"
+
+BOOL sub_0200A328 (const UnkStruct_02009DC8 * param0)
+{
+    UnkStruct_0200A328 v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 0);
+
+    v0.unk_00 = sub_02009E1C(param0);
+    v0.unk_04 = sub_02009EBC(param0);
+    v0.unk_08 = sub_02009E08(param0);
+    v0.unk_0C = 0;
+
+    return sub_0201EA24(&v0);
+}
+
+void sub_0200A368 (const UnkStruct_02009CFC * param0)
+{
+    int v0;
+    BOOL v1;
+
+    GF_ASSERT(param0);
+
+    for (v0 = 0; v0 < param0->unk_04; v0++) {
+        if (param0->unk_00[v0]) {
+            v1 = sub_0200A328(param0->unk_00[v0]);
+            GF_ASSERT(v1);
+        }
+    }
+}
+
+BOOL sub_0200A39C (const UnkStruct_02009DC8 * param0)
+{
+    UnkStruct_0200A328 v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 0);
+
+    v0.unk_00 = sub_02009E1C(param0);
+    v0.unk_04 = sub_02009EBC(param0);
+    v0.unk_08 = sub_02009E08(param0);
+    v0.unk_0C = 0;
+
+    return sub_0201EA7C(&v0);
+}
+
+BOOL sub_0200A3DC (const UnkStruct_02009DC8 * param0)
+{
+    UnkStruct_0200A328 v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 0);
+
+    v0.unk_00 = sub_02009E1C(param0);
+    v0.unk_04 = sub_02009EBC(param0);
+    v0.unk_08 = sub_02009E08(param0);
+    v0.unk_0C = 1;
+
+    return sub_0201EA24(&v0);
+}
+
+void sub_0200A41C (const UnkStruct_02009CFC * param0)
+{
+    int v0;
+    BOOL v1;
+
+    GF_ASSERT(param0);
+
+    for (v0 = 0; v0 < param0->unk_04; v0++) {
+        if (param0->unk_00[v0]) {
+            v1 = sub_0200A3DC(param0->unk_00[v0]);
+            GF_ASSERT(v1);
+        }
+    }
+}
+
+BOOL sub_0200A450 (const UnkStruct_02009DC8 * param0)
+{
+    UnkStruct_0200A328 v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 0);
+
+    v0.unk_00 = sub_02009E1C(param0);
+    v0.unk_04 = sub_02009EBC(param0);
+    v0.unk_08 = sub_02009E08(param0);
+    v0.unk_0C = 1;
+
+    return sub_0201EA7C(&v0);
+}
+
+void sub_0200A490 (const UnkStruct_02009DC8 * param0, const UnkStruct_02009DC8 * param1)
+{
+    int v0;
+    NNSG2dCharacterData * v1;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(param1);
+
+    v0 = sub_02009E08(param0);
+    v1 = sub_02009E1C(param1);
+
+    sub_0201EB08(v0, v1);
+}
+
+void sub_0200A4C0 (const UnkStruct_02009DC8 * param0)
+{
+    int v0;
+    NNSG2dCharacterData * v1;
+
+    GF_ASSERT(param0);
+
+    v0 = sub_02009E08(param0);
+    v1 = sub_02009E1C(param0);
+
+    sub_0201EB08(v0, v1);
+}
+
+void sub_0200A4E4 (UnkStruct_02009DC8 * param0)
+{
+    int v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 0);
+
+    v0 = sub_02009E08(param0);
+    sub_0201EB50(v0);
+}
+
+void sub_0200A508 (UnkStruct_02009CFC * param0)
+{
+    int v0;
+
+    GF_ASSERT(param0);
+
+    for (v0 = 0; v0 < param0->unk_04; v0++) {
+        if (param0->unk_00[v0]) {
+            sub_0200A4E4(param0->unk_00[v0]);
+        }
+    }
+}
+
+NNSG2dImageProxy * sub_0200A534 (const UnkStruct_02009DC8 * param0)
+{
+    int v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 0);
+
+    v0 = sub_02009E08(param0);
+    return sub_0201EBDC(v0);
+}
+
+NNSG2dImageProxy * sub_0200A558 (const UnkStruct_02009DC8 * param0, const UnkStruct_02009DC8 * param1)
+{
+    int v0;
+    NNSG2dCellDataBank * v1;
+    NNSG2dImageProxy * v2;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 0);
+    GF_ASSERT(param1);
+    GF_ASSERT(sub_02009EAC(param1) == 2);
+
+    v0 = sub_02009E08(param0);
+    v1 = sub_02009E4C(param1);
+    v2 = sub_0201EC00(v0, v1->pVramTransferData->szByteMax);
+
+    if (v2 == NULL) {
+        NNSG2dImageProxy * v3;
+
+        v3 = sub_0201EBDC(v0);
+        v2 = sub_0201EC84(v3);
+    }
+
+    return v2;
+}
+
+void sub_0200A5B4 (const NNSG2dImageProxy * param0)
+{
+    GF_ASSERT(param0);
+    sub_0201ED1C(param0);
+}
+
+BOOL sub_0200A5C8 (const UnkStruct_02009DC8 * param0)
+{
+    UnkStruct_0200A5C8 v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 1);
+
+    v0.unk_00 = sub_02009E34(param0);
+    v0.unk_04 = sub_02009EBC(param0);
+    v0.unk_0C = sub_02009E08(param0);
+    v0.unk_08 = sub_02009EE8(param0);
+
+    return sub_0201F90C(&v0);
+}
+
+void sub_0200A60C (const UnkStruct_02009CFC * param0)
+{
+    int v0;
+    BOOL v1;
+
+    GF_ASSERT(param0);
+
+    for (v0 = 0; v0 < param0->unk_04; v0++) {
+        if (param0->unk_00[v0]) {
+            v1 = sub_0200A5C8(param0->unk_00[v0]);
+            GF_ASSERT(v1);
+        }
+    }
+}
+
+BOOL sub_0200A640 (const UnkStruct_02009DC8 * param0)
+{
+    UnkStruct_0200A5C8 v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 1);
+
+    v0.unk_00 = sub_02009E34(param0);
+    v0.unk_04 = sub_02009EBC(param0);
+    v0.unk_0C = sub_02009E08(param0);
+    v0.unk_08 = sub_02009EE8(param0);
+
+    return sub_0201F950(&v0);
+}
+
+void sub_0200A684 (const UnkStruct_02009CFC * param0)
+{
+    int v0;
+    BOOL v1;
+
+    GF_ASSERT(param0);
+
+    for (v0 = 0; v0 < param0->unk_04; v0++) {
+        if (param0->unk_00[v0]) {
+            v1 = sub_0200A640(param0->unk_00[v0]);
+            GF_ASSERT(v1);
+        }
+    }
+}
+
+void sub_0200A6B8 (const UnkStruct_02009DC8 * param0)
+{
+    NNSG2dPaletteData * v0;
+    int v1;
+    int v2;
+
+    GF_ASSERT(param0);
+
+    v1 = sub_02009E08(param0);
+    v0 = sub_02009E34(param0);
+
+    sub_0201F990(v1, v0);
+}
+
+void sub_0200A6DC (UnkStruct_02009DC8 * param0)
+{
+    int v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 1);
+
+    v0 = sub_02009E08(param0);
+    sub_0201F9F0(v0);
+}
+
+void sub_0200A700 (UnkStruct_02009CFC * param0)
+{
+    int v0;
+
+    GF_ASSERT(param0);
+
+    for (v0 = 0; v0 < param0->unk_04; v0++) {
+        if (param0->unk_00[v0]) {
+            sub_0200A6DC(param0->unk_00[v0]);
+        }
+    }
+}
+
+const NNSG2dImagePaletteProxy * sub_0200A72C (const UnkStruct_02009DC8 * param0, NNSG2dImageProxy * param1)
+{
+    int v0;
+
+    GF_ASSERT(param0);
+    GF_ASSERT(sub_02009EAC(param0) == 1);
+
+    v0 = sub_02009E08(param0);
+
+    if (param1) {
+        return sub_0201FA80(v0, param1);
+    }
+
+    return sub_0201FA58(v0);
+}
+
+u32 sub_0200A760 (const UnkStruct_02009DC8 * param0, NNS_G2D_VRAM_TYPE param1)
+{
+    int v0;
+    const NNSG2dImagePaletteProxy * v1;
+
+    v1 = sub_0200A72C(param0, NULL);
+    v0 = NNS_G2dGetImagePaletteLocation(v1, param1);
+
+    if (v0 != (NNS_G2D_VRAM_ADDR_NOT_INITIALIZED)) {
+        v0 /= 32;
+    }
+
+    return v0;
+}
