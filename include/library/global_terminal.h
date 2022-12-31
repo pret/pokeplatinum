743bf8d6eb028fc7bc70536fcc53918c422c4aa0
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2022-12-30 23:15:00 -0500

    Create global_terminal.h

---
+++ include/library/global_terminal.h
@@ -0,0 +1,41 @@
+#ifndef POKEPLATINUM_GLOBAL_TERMINAL_H
+#define POKEPLATINUM_GLOBAL_TERMINAL_H
+
+#include "overlay062/struct_ov62_022349A8_sub1.h"
+#include "overlay062/struct_ov62_022349A8_sub3_sub1.h"
+#include "overlay062/struct_ov62_022349A8_sub3_sub2.h"
+#include "overlay062/struct_ov62_0223D518_sub1.h"
+#include "overlay062/struct_ov62_022349A8_sub3_sub3.h"
+#include "overlay062/struct_ov62_022349A8_sub3_sub4.h"
+#include "overlay062/struct_ov62_022349A8_sub3_sub5.h"
+#include "overlay062/struct_ov62_022349A8_sub3_sub5.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BOOL ov61_0222DCDC(UnkStruct_ov62_022349A8_sub1 * param0);
+void ov61_0222DCFC(void);
+int ov61_0222DD08(void);
+int ov61_0222DD24(void);
+BOOL ov61_0222DD30(UnkStruct_ov62_022349A8_sub3_sub1 * param0, void * param1);
+BOOL ov61_0222DD8C(long param0, void * param1);
+BOOL ov61_0222DDCC(long param0, UnkStruct_ov62_022349A8_sub3_sub2 * param1, void * param2);
+BOOL ov61_0222DE34(long param0, void * param1);
+BOOL ov61_0222DE98(void * param0);
+BOOL ov61_0222DEB8(UnkStruct_ov62_0223D518_sub1 * param0, void * param1);
+void * ov61_0222DE80(void);
+long ov61_0222DE74(void);
+long ov61_0222DE8C(long param0);
+BOOL ov61_0222DF08(UnkStruct_ov62_022349A8_sub3_sub3 * param0, void * param1);
+BOOL ov61_0222DF60(UnkStruct_ov62_022349A8_sub3_sub4 * param0, void * param1);
+BOOL ov61_0222DFA8(UnkStruct_ov62_022349A8_sub3_sub5 * param0, void * param1);
+BOOL ov61_0222E000(UnkStruct_ov62_022349A8_sub3_sub5 * param0, void * param1);
+BOOL ov61_0222E058(u64 param0, u32 param1, void * param2);
+BOOL ov61_0222E08C(u64 param0, void * param1);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // POKEPLATINUM_GLOBAL_TERMINAL_H
