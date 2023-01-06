f8a69d39d540d97c1d2a7ed7f4bd7796fbd979de
Nomura <72407407+Nomura-RH@users.noreply.github.com>
2023-01-06 15:01:02 +0100

    Match unk_ov1_021D0D80.c

---
+++ src/overlay001/ov1_021D0D80.c
@@ -0,0 +1,16 @@
+#include <nitro.h>
+#include <string.h>
+#include <nitro/sinit.h>
+
+void include_unk_ov1_021D0DA0(void);
+
+void include_unk_ov1_021D0DA0 (void)
+{
+    static int unk_ov1_021D0DA0 = 0;
+}
+
+static void NitroStaticInit (void)
+{
+    static int v0 = 0;
+    v0++;
+}
