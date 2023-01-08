689360abd317e4c31f744a843b3e0244c9b211f8
Nomura <72407407+Nomura-RH@users.noreply.github.com>
2023-01-08 22:29:05 +0100

    Match NitroSystem/fnd/allocator.c

--- force_active.mk
+++ force_active.mk
@@ -1,9 +1,6 @@
 FORCE_ACTIVE_SYMBOLS := \
 	DGTi_hash2_arm4_small, \
 	Exception_Table_Index, \
-	Unk_020F9550, \
-	Unk_020F9558, \
-	Unk_020F9568, \
 	Unk_020F9630, \
 	Unk_020F9634, \
 	Unk_020F9638, \
