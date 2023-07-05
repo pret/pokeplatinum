18110f84844709074b39ac27d366fa6a341693a0
Lhea (Rachel) <lhearachel@proton.me>
2023-07-05 01:38:55 -0700

    Use bitshifts for battle type constants

--- include/constants/battle.h
+++ include/constants/battle.h
@@ -1,18 +1,18 @@
 #ifndef POKEPLATINUM_CONSTANTS_BATTLE_H
 #define POKEPLATINUM_CONSTANTS_BATTLE_H
 
-#define BATTLE_TYPE_SINGLES         (0x00000000)
-#define BATTLE_TYPE_WILD_MON        (0x00000000)
-#define BATTLE_TYPE_TRAINER         (0x00000001)
-#define BATTLE_TYPE_DOUBLES         (0x00000002)
-#define BATTLE_TYPE_LINK            (0x00000004)
-#define BATTLE_TYPE_2vs2            (0x00000008)    // This is, explicitly, two distinct battlers on each side
-#define BATTLE_TYPE_TAG             (0x00000010)    // This is, explicitly, a battle with a partner
-#define BATTLE_TYPE_SAFARI          (0x00000020)
-#define BATTLE_TYPE_AI              (0x00000040)
-#define BATTLE_TYPE_FRONTIER        (0x00000080)
-#define BATTLE_TYPE_ROAMER          (0x00000100)
-#define BATTLE_TYPE_PAL_PARK        (0x00000200)
-#define BATTLE_TYPE_CATCH_TUTORIAL  (0x00000400)
+#define BATTLE_TYPE_SINGLES         (0 << 0)
+#define BATTLE_TYPE_WILD_MON        (0 << 0)
+#define BATTLE_TYPE_TRAINER         (1 << 0)
+#define BATTLE_TYPE_DOUBLES         (1 << 1)
+#define BATTLE_TYPE_LINK            (1 << 2)
+#define BATTLE_TYPE_2vs2            (1 << 3) // This is, explicitly, two distinct battlers on each side
+#define BATTLE_TYPE_TAG             (1 << 4) // This is, explicitly, a battle with a partner
+#define BATTLE_TYPE_SAFARI          (1 << 5)
+#define BATTLE_TYPE_AI              (1 << 6)
+#define BATTLE_TYPE_FRONTIER        (1 << 7)
+#define BATTLE_TYPE_ROAMER          (1 << 8)
+#define BATTLE_TYPE_PAL_PARK        (1 << 9)
+#define BATTLE_TYPE_CATCH_TUTORIAL  (1 << 10)
 
 #endif // POKEPLATINUM_CONSTANTS_BATTLE_H
