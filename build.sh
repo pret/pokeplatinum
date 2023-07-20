6f7185a311ad1ca2f9cf3e3c22364eb273ef9306
mid-kid <esteve.varela@gmail.com>
2023-07-20 03:47:43 +0200

    Jerry-rig fixrom into the pokeplatinum target by using a shell snippet
    This is as "proper" as it gets. The alternative is using an intermediate file.

--- build.sh
+++ build.sh
@@ -3,18 +3,4 @@
 # Set up env variable to show % of completion during ninja build
 export NINJA_STATUS="[%p %f/%t] "
 
-cd build
-
-# Build rom
-ninja
-
-export NINJA_STATUS=""
-
-# Fix rom header secure area CRC
-ninja fixrom
-
-export NINJA_STATUS="
-"
-
-# Checksum verification
-ninja test
+ninja -C build test
