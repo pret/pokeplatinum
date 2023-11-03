522cd4e846cec5a4f7674ce23348b5c33a6fb18e
Khandoker Munif Ahmed <kmunifahmed@gmail.com>
2023-11-02 17:55:29 -0700

    Make suggested chagnes

--- tools/json2bin/evo.py
+++ tools/json2bin/evo.py
@@ -11,52 +11,52 @@ from consts import (
 )
 
 
-consts.pokemon import (
+from consts.pokemon import (
     evo_methods
     species
 )
 
 
-def get_evo_params(method: EvoMethod, evo: list):
+def get_evo_params(method: evo_methods.EvoMethod, evo: list):
     maybe_param = evo[1]
     final_param = 0
     #None of these take an extra parameter
     if method in set([
-        evo_methods.EvoMethod.NONE,
-        evo_methods.EvoMethod.HAPPINESS,
-        evo_methods.EvoMethod.HAPPINESS_DAY,
-        evo_methods.EvoMethod.HAPPINESS_NIGHT,
-        evo_methods.EvoMethod.TRADE,
-        evo_methods.EvoMethod.ELECTRIC_FIELD,
-        evo_methods.EvoMethod.MOSSY_STONE,
-        evo_methods.EvoMethod.ICY_STONE]):
+        evo_methods.EvoMethod.EVO_NONE,
+        evo_methods.EvoMethod.EVO_HAPPINESS,
+        evo_methods.EvoMethod.EVO_HAPPINESS_DAY,
+        evo_methods.EvoMethod.EVO_HAPPINESS_NIGHT,
+        evo_methods.EvoMethod.EVO_TRADE,
+        evo_methods.EvoMethod.EVO_ELECTRIC_FIELD,
+        evo_methods.EvoMethod.EVO_MOSSY_STONE,
+        evo_methods.EvoMethod.EVO_ICY_STONE]):
         final_param = 0
     # These all specify a basic integer param
     elif method in set([
-        evo_methods.EvoMethod.LEVEL_UP,
-        evo_methods.EvoMethod.LEVEL_ATK_GT_DEF,
-        evo_methods.EvoMethod.LEVEL_ATK_EQ_DEF,
-        evo_methods.EvoMethod.LEVEL_ATK_LT_DEF,
-        evo_methods.EvoMethod.LEVEL_PID_LOW,
-        evo_methods.EvoMethod.LEVEL_PID_HIGH,
-        evo_methods.EvoMethod.LEVEL_NINJASK,
-        evo_methods.EvoMethod.LEVEL_SHEDINJA,
-        evo_methods.EvoMethod.LEVEL_MALE,
-        evo_methods.EvoMethod.LEVEL_FEMALE,
-        evo_methods.EvoMethod.BEAUTY]):
+        evo_methods.EvoMethod.EVO_LEVEL_UP,
+        evo_methods.EvoMethod.EVO_LEVEL_ATK_GT_DEF,
+        evo_methods.EvoMethod.EVO_LEVEL_ATK_EQ_DEF,
+        evo_methods.EvoMethod.EVO_LEVEL_ATK_LT_DEF,
+        evo_methods.EvoMethod.EVO_LEVEL_PID_LOW,
+        evo_methods.EvoMethod.EVO_LEVEL_PID_HIGH,
+        evo_methods.EvoMethod.EVO_LEVEL_NINJASK,
+        evo_methods.EvoMethod.EVO_LEVEL_SHEDINJA,
+        evo_methods.EvoMethod.EVO_LEVEL_MALE,
+        evo_methods.EvoMethod.EVO_LEVEL_FEMALE,
+        evo_methods.EvoMethod.EVO_BEAUTY]):
         final_param = maybe_param
     # These specify an item
     elif method in set([
-        evo_methods.EvoMethod.TRADE_WITH_ITEM,
-        evo_methods.EvoMethod.USE_ITEM,
-        evo_methods.EvoMethod.USE_ITEM_MALE,
-        evo_methods.EvoMethod.USE_ITEM_FEMALE,
-        evo_methods.EvoMethod.USE_ITEM_DAY,
-        evo_methods.EvoMethod.USE_ITEM_NIGHT]):
+        evo_methods.EvoMethod.EVO_TRADE_WITH_ITEM,
+        evo_methods.EvoMethod.EVO_USE_ITEM,
+        evo_methods.EvoMethod.EVO_USE_ITEM_MALE,
+        evo_methods.EvoMethod.EVO_USE_ITEM_FEMALE,
+        evo_methods.EvoMethod.EVO_USE_ITEM_DAY,
+        evo_methods.EvoMethod.EVO_USE_ITEM_NIGHT]):
         final_param = item.Item[maybe_param].value
-    elif method == EvoMethod.KNOW_MOVE:
+    elif method == EvoMethod.EVO_KNOW_MOVE:
         final_param = moves.Move[maybe_param].value
-    elif method == EvoMethod.MON_IN_PARTY:
+    elif method == EvoMethod.EVO_MON_IN_PARTY:
         final_param = species.Species[maybe_param].value
 
     return final_param
@@ -64,9 +64,9 @@ def get_evo_params(method: EvoMethod, evo: list):
 
 def table_line(evo_method: int, evo_params: int, species: int) -> bytes:
     binary = bytearray([])
-    binary = binary + method.to_bytes(2, 'little')
-    binary = binary + params.to_bytes(2, 'little')
-    binary = binary + target.to_bytes(2, 'little')
+    binary.extend(method.to_bytes(2, 'little'))
+    binary.extend(params.to_bytes(2, 'little'))
+    binary.extend(target.to_bytes(2, 'little'))
     return bytes(binary)
     
 
