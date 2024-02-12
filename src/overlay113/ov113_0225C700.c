b14163ed9190c1ac2b9c92e43e6832c08316bb8f
Bepis <36346617+bbepis@users.noreply.github.com>
2024-02-13 06:34:31 +1100

    Generate footprint size data table

--- src/overlay113/ov113_0225C700.c
+++ src/overlay113/ov113_0225C700.c
@@ -1141,7 +1141,7 @@ static BOOL ov113_0225D938 (int param0, int param1, CellActorData * param2, NARC
         return 0;
     }
 
-    if (ov113_02260788(param0, param1, param6) == 1) {
+    if (PokemonHasOverworldFootprint(param0, param1, param6) == 1) {
         v2 = sub_0200723C(param4, 3 + param0, 1, 118, 1);
         NNS_G2dGetUnpackedCharacterData(v2, &v3);
         DC_FlushRange(v3->pRawData, 0x20 * 8);
