3ae55730cb7a3ddb11e264cf56545d3eba80d53d
Trainer Riley <rnbminoriv@gmail.com>
2024-05-31 18:53:35 -0700

    Remove (accidentally re-added) old field system header

--- src/overlay025/poketch_system.c
+++ src/overlay025/poketch_system.c
@@ -21,7 +21,7 @@
 #include "heap.h"
 #include "unk_0201E3D8.h"
 #include "unk_02025E08.h"
-#include "unk_0203CC84.h"
+#include "field_system.h"
 #include "trainer_info.h"
 #include "unk_020508D4.h"
 #include "unk_02099D44.h"
