46ec3a3a131cf00cd15181cc45cb435fcf23e080
Rachel <lhearachel@proton.me>
2024-06-21 11:33:33 -0700

    Document Encode/DecodeGameRecords

--- src/game_records.c
+++ src/game_records.c
@@ -1,6 +1,8 @@
 #include <nitro.h>
 #include <string.h>
 
+#include "consts/game_records.h"
+
 #include "struct_decls/pokedexdata_decl.h"
 
 #include "unk_0201D15C.h"
@@ -8,8 +10,11 @@
 #include "unk_0202631C.h"
 #include "game_records.h"
 
-static void sub_0202CD94(GameRecords * param0, int param1);
-static void sub_0202CDC0(GameRecords * param0, int param1);
+#define START_ENCODED_RECORDS   RECORD_UNK_001
+#define SIZE_ENCODED_RECORDS    (sizeof(GameRecords) - sizeof(EncodingSeed) - (START_ENCODED_RECORDS * sizeof(u32)))
+
+static void EncodeGameRecords(GameRecords *records, int id);
+static void DecodeGameRecords(GameRecords *records, int id);
 
 int GameRecords_SaveSize(void)
 {
@@ -21,7 +26,7 @@ void GameRecords_Init(GameRecords *records)
     MI_CpuClear32(records, sizeof(GameRecords));
     records->seed.modifier = OS_GetVBlankCount() | (OS_GetVBlankCount() << 8);
 
-    sub_0202CD94(records, 1);
+    EncodeGameRecords(records, START_ENCODED_RECORDS);
 }
 
 GameRecords *SaveData_GetGameRecordsPtr(SaveData *savedata)
@@ -29,23 +34,29 @@ GameRecords *SaveData_GetGameRecordsPtr(SaveData *savedata)
     return SaveData_SaveTable(savedata, SAVE_TABLE_ENTRY_GAME_RECORDS);
 }
 
-static void sub_0202CD94 (GameRecords * records, int param1)
+// this inline does not match when operating on the substruct
+static inline u32 HashEncodingSeed(GameRecords *records)
 {
-    if (param1 == (0 + 0)) {
-        return;
-    }
-
-    records->seed.byteSum = SumBytes(&records->recordsU32[1], sizeof(GameRecords) - sizeof(EncodingSeed) - sizeof(u32)) & 0xffff;
-    EncodeData(&records->recordsU32[1], sizeof(GameRecords) - sizeof(EncodingSeed) - sizeof(u32), records->seed.byteSum + (records->seed.modifier << 16));
+    return records->seed.byteSum + (records->seed.modifier << 16);
 }
 
-static void sub_0202CDC0 (GameRecords * records, int param1)
+static void EncodeGameRecords(GameRecords *records, int id)
 {
-    if (param1 == (0 + 0)) {
+    if (id == RECORD_UNK_000) {
         return;
     }
 
-    DecodeData(&records->recordsU32[1], sizeof(GameRecords) - sizeof(EncodingSeed) - sizeof(u32), records->seed.byteSum + (records->seed.modifier << 16));
+    records->seed.byteSum = SumBytes(&records->recordsU32[START_ENCODED_RECORDS], SIZE_ENCODED_RECORDS) & 0xFFFF;
+    EncodeData(&records->recordsU32[START_ENCODED_RECORDS], SIZE_ENCODED_RECORDS, HashEncodingSeed(records));
+}
+
+static void DecodeGameRecords(GameRecords *records, int id)
+{
+    if (id == RECORD_UNK_000) {
+        return;
+    }
+
+    DecodeData(&records->recordsU32[START_ENCODED_RECORDS], SIZE_ENCODED_RECORDS, HashEncodingSeed(records));
 }
 
 static u32 sub_0202CDE0 (const GameRecords * records, int param1)
@@ -279,7 +290,7 @@ u32 sub_0202CE90 (GameRecords * records, int param1, u32 param2)
     u32 v0 = sub_0202CE3C(param1);
     u32 v1;
 
-    sub_0202CDC0(records, param1);
+    DecodeGameRecords(records, param1);
 
     if (param2 < v0) {
         v1 = sub_0202CE08(records, param1, param2);
@@ -287,7 +298,7 @@ u32 sub_0202CE90 (GameRecords * records, int param1, u32 param2)
         v1 = sub_0202CE08(records, param1, v0);
     }
 
-    sub_0202CD94(records, param1);
+    EncodeGameRecords(records, param1);
 
     return v1;
 }
@@ -298,7 +309,7 @@ u32 sub_0202CED0 (GameRecords * records, int param1, u32 param2)
     u32 v1;
     u32 v2;
 
-    sub_0202CDC0(records, param1);
+    DecodeGameRecords(records, param1);
 
     v1 = sub_0202CDE0(records, param1);
     v2 = v1;
@@ -315,7 +326,7 @@ u32 sub_0202CED0 (GameRecords * records, int param1, u32 param2)
         }
     }
 
-    sub_0202CD94(records, param1);
+    EncodeGameRecords(records, param1);
 
     return v2;
 }
@@ -326,7 +337,7 @@ u32 sub_0202CF28 (GameRecords * records, int param1)
     u32 v1;
     u32 v2;
 
-    sub_0202CDC0(records, param1);
+    DecodeGameRecords(records, param1);
 
     v1 = sub_0202CDE0(records, param1);
 
@@ -336,7 +347,7 @@ u32 sub_0202CF28 (GameRecords * records, int param1)
         v2 = sub_0202CE08(records, param1, v0);
     }
 
-    sub_0202CD94(records, param1);
+    EncodeGameRecords(records, param1);
 
     return v2;
 }
@@ -347,7 +358,7 @@ u32 sub_0202CF70 (GameRecords * records, int param1, u32 param2)
     u32 v1;
     u32 v2;
 
-    sub_0202CDC0(records, param1);
+    DecodeGameRecords(records, param1);
 
     v1 = sub_0202CDE0(records, param1);
 
@@ -357,7 +368,7 @@ u32 sub_0202CF70 (GameRecords * records, int param1, u32 param2)
         v2 = sub_0202CE08(records, param1, v0);
     }
 
-    sub_0202CD94(records, param1);
+    EncodeGameRecords(records, param1);
 
     return v2;
 }
@@ -367,9 +378,9 @@ u32 sub_0202CFB8 (GameRecords * records, int param1)
     u32 v0 = sub_0202CE3C(param1);
     u32 v1;
 
-    sub_0202CDC0(records, param1);
+    DecodeGameRecords(records, param1);
     v1 = sub_0202CDE0(records, param1);
-    sub_0202CD94(records, param1);
+    EncodeGameRecords(records, param1);
 
     if (v1 > v0) {
         return v0;
