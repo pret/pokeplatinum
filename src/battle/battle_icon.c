ec4f1ffc50c60b2f5fc900e24896a130fdbb7314
Joshua Smith <joshua.barton.smith12@gmail.com>
2024-10-16 13:49:14 -0500

    rename all terrain img/pals, better describe arrays using pl_batt_obj data

--- src/battle/battle_icon.c
+++ src/battle/battle_icon.c
@@ -8,83 +8,92 @@
 #include "struct_defs/sprite_template.h"
 #include "struct_defs/struct_0200D0F4.h"
 
+#include "consts/pokemon.h"
+#include "consts/moves.h"
 #include "palette.h"
 #include "unk_0200C6E4.h"
+
+// these do not align with the types defined in consts/pokemon.json.
+#define TYPE_COOL_ICON NUMBER_OF_MON_TYPES + 0
+#define TYPE_BEAUTY_ICON NUMBER_OF_MON_TYPES + 1
+#define TYPE_CUTE_ICON NUMBER_OF_MON_TYPES + 2
+#define TYPE_SMART_ICON NUMBER_OF_MON_TYPES + 3
+#define TYPE_TOUGH_ICON NUMBER_OF_MON_TYPES + 4
 
 /*
  * Maps a type index (e.g. TYPE_WATER) to the corresponding NARC member in pl_batt_obj.narc containing the type icon.
  */
-__attribute__((aligned(4))) static const u32 BattleIcon_NARCMemberMap[] = {
-    0xEA,
-    0xE1,
-    0xE3,
-    0xEB,
-    0xE5,
-    0xED,
-    0xE7,
-    0xE4,
-    0xEE,
-    0xEC,
-    0xE2,
-    0xF1,
-    0xE9,
-    0xDE,
-    0xDF,
-    0xE6,
-    0xDD,
-    0xE0,
-    0xF0,
-    0xDB,
-    0xDC,
-    0xE8,
-    0xEF
+__attribute__((aligned(4))) static const u32 sBattleIcon_NARCMemberMap[] = {
+    [TYPE_NORMAL] = 0xEA,
+    [TYPE_FIGHTING] = 0xE1,
+    [TYPE_FLYING] = 0xE3,
+    [TYPE_POISON] = 0xEB,
+    [TYPE_GROUND] = 0xE5,
+    [TYPE_ROCK] = 0xED,
+    [TYPE_BUG] = 0xE7,
+    [TYPE_GHOST] = 0xE4,
+    [TYPE_STEEL] = 0xEE,
+    [TYPE_MYSTERY] = 0xEC,
+    [TYPE_FIRE] = 0xE2,
+    [TYPE_WATER] = 0xF1,
+    [TYPE_GRASS] = 0xE9,
+    [TYPE_ELECTRIC] = 0xDE,
+    [TYPE_PSYCHIC] = 0xDF,
+    [TYPE_ICE] = 0xE6,
+    [TYPE_DRAGON] = 0xDD,
+    [TYPE_DARK] = 0xE0,
+    [TYPE_COOL_ICON] = 0xF0,
+    [TYPE_BEAUTY_ICON] = 0xDB,
+    [TYPE_CUTE_ICON] = 0xDC,
+    [TYPE_SMART_ICON] = 0xE8,
+    [TYPE_TOUGH_ICON] = 0xEF
 };
 
 /*
  * Maps a type index (e.g. TYPE_WATER) to the corresponding palette index in NARC_INDEX_BATTLE__GRAPHIC__PL_BATT_OBJ__TYPE_PALETTE.
  */
-__attribute__((aligned(4))) static const u8 BattleIcon_PaletteIndexMap[] = {
-    0x0,
-    0x0,
-    0x1,
-    0x1,
-    0x0,
-    0x0,
-    0x2,
-    0x1,
-    0x0,
-    0x2,
-    0x0,
-    0x1,
-    0x2,
-    0x0,
-    0x1,
-    0x1,
-    0x2,
-    0x0,
-    0x0,
-    0x1,
-    0x1,
-    0x2,
-    0x0
+__attribute__((aligned(4))) static const u8 sBattleIcon_PaletteIndexMap[] = {
+    [TYPE_NORMAL] = 0x0,
+    [TYPE_FIGHTING] = 0x0,
+    [TYPE_FLYING] = 0x1,
+    [TYPE_POISON] = 0x1,
+    [TYPE_GROUND] = 0x0,
+    [TYPE_ROCK] = 0x0,
+    [TYPE_BUG] = 0x2,
+    [TYPE_GHOST] = 0x1,
+    [TYPE_STEEL] = 0x0,
+    [TYPE_MYSTERY] = 0x2,
+    [TYPE_FIRE] = 0x0,
+    [TYPE_WATER] = 0x1,
+    [TYPE_GRASS] = 0x2,
+    [TYPE_ELECTRIC] = 0x0,
+    [TYPE_PSYCHIC] = 0x1,
+    [TYPE_ICE] = 0x1,
+    [TYPE_DRAGON] = 0x2,
+    [TYPE_DARK] = 0x0,
+    [TYPE_COOL_ICON] = 0x0,
+    [TYPE_BEAUTY_ICON] = 0x1,
+    [TYPE_CUTE_ICON] = 0x1,
+    [TYPE_SMART_ICON] = 0x2,
+    [TYPE_TOUGH_ICON] = 0x0
 };
 
 /*
- * Maps move types (Physical, Special, Status) to the corresponding NARC member in pl_batt_obj.narc containing the type icon.
+ * Maps move classes (Physical, Special, Status) to the corresponding NARC member in pl_batt_obj.narc containing the type icon.
  */
-__attribute__((aligned(4))) static const u32 BattleIcon_MoveType_NARCMemberMap[] = {
-    0xF4,
-    0xF6,
-    0xF5
+__attribute__((aligned(4))) static const u32 sBattleIcon_MoveClass_NARCMemberMap[] = {
+    [CLASS_PHYSICAL] = 0xF4,
+    [CLASS_SPECIAL] = 0xF6,
+    [CLASS_STATUS] = 0xF5
 };
 
 /*
- * Maps a move type (Physical, Special, Status) to the corresponding palette index in NARC_INDEX_BATTLE__GRAPHIC__PL_BATT_OBJ__TYPE_PALETTE.
+ * Maps move classes (Physical, Special, Status) to the corresponding palette index in NARC_INDEX_BATTLE__GRAPHIC__PL_BATT_OBJ__TYPE_PALETTE.
  */
-__attribute__((aligned(4))) static const u8 BattleIcon_MoveType_PaletteIndexMap[] = {
-    0x0,
-    0x1,
-    0x0
+__attribute__((aligned(4))) static const u8 sBattleIcon_MoveClass_PaletteIndexMap[] = {
+    [CLASS_PHYSICAL] = 0x0,
+    [CLASS_SPECIAL] = 0x1,
+    [CLASS_STATUS] = 0x0
 };
 
 /*
@@ -93,8 +102,8 @@ __attribute__((aligned(4))) static const u8 BattleIcon_MoveType_PaletteIndexMap[
  */
 u32 BattleIcon_TypeIndexToNARCMember(int typeIndex)
 {
-    GF_ASSERT(typeIndex < NELEMS(BattleIcon_NARCMemberMap));
-    return BattleIcon_NARCMemberMap[typeIndex];
+    GF_ASSERT(typeIndex < NELEMS(sBattleIcon_NARCMemberMap));
+    return sBattleIcon_NARCMemberMap[typeIndex];
 }
 
 /*
@@ -122,8 +131,8 @@ u32 sub_0207C928(void)
  */
 u8 BattleIcon_TypeIndexToPaletteIndex(int typeIndex)
 {
-    GF_ASSERT(typeIndex < NELEMS(BattleIcon_PaletteIndexMap));
-    return BattleIcon_PaletteIndexMap[typeIndex];
+    GF_ASSERT(typeIndex < NELEMS(sBattleIcon_PaletteIndexMap));
+    return sBattleIcon_PaletteIndexMap[typeIndex];
 }
 
 /*
@@ -189,37 +198,37 @@ void sub_0207CA88(CellActorData *param0)
 }
 
 /*
- * Given a movetype index, return the corresponding NARC member containing the icon for that movetype.
+ * Given a move class index, return the corresponding NARC member containing the icon for that move class.
  * See also BattleIcon_GetNARCIndex.
  */
-u32 BattleIcon_MoveTypeIndexToNARCMember(int moveTypeIndex)
+u32 BattleIcon_MoveClassIndexToNARCMember(int moveClassIndex)
 {
-    GF_ASSERT(moveTypeIndex < NELEMS(BattleIcon_MoveType_NARCMemberMap));
-    return BattleIcon_MoveType_NARCMemberMap[moveTypeIndex];
+    GF_ASSERT(moveClassIndex < NELEMS(sBattleIcon_MoveClass_NARCMemberMap));
+    return sBattleIcon_MoveClass_NARCMemberMap[moveClassIndex];
 }
 
 /*
- * Given a movetype index, return the corresponding palette index to use for that movetype's icon.
+ * Given a move class index, return the corresponding palette index to use for that move class' icon.
  * See also BattleIcon_GetNARCIndex and BattleIcon_GetPaletteNARCMember.
  */
-u8 BattleIcon_MoveTypeIndexToPaletteIndex(int moveTypeIndex)
+u8 BattleIcon_MoveClassIndexToPaletteIndex(int moveClassIndex)
 {
-    GF_ASSERT(moveTypeIndex < NELEMS(BattleIcon_MoveType_PaletteIndexMap));
-    return BattleIcon_MoveType_PaletteIndexMap[moveTypeIndex];
+    GF_ASSERT(moveClassIndex < NELEMS(sBattleIcon_MoveClass_PaletteIndexMap));
+    return sBattleIcon_MoveClass_PaletteIndexMap[moveClassIndex];
 }
 
 /*
  * Returns the index of the NARC for pl_batt_obj.
  * (This is identical to BattleIcon_GetNARCIndex).
  */
-u32 BattleIcon_GetMoveTypeNARCIndex(void)
+u32 BattleIcon_GetMoveClassNARCIndex(void)
 {
     return NARC_INDEX_BATTLE__GRAPHIC__PL_BATT_OBJ;
 }
 
 void BattleIcon_MakeMoveTypeSpriteTiles(SpriteRenderer *param0, SpriteGfxHandler *param1, NNS_G2D_VRAM_TYPE param2, int moveTypeIndex, u32 param4)
 {
-    sub_0200CBDC(param0, param1, BattleIcon_GetMoveTypeNARCIndex(), BattleIcon_MoveTypeIndexToNARCMember(moveTypeIndex), 1, param2, param4);
+    sub_0200CBDC(param0, param1, BattleIcon_GetMoveClassNARCIndex(), BattleIcon_MoveClassIndexToNARCMember(moveTypeIndex), 1, param2, param4);
 }
 
 void sub_0207CAF8(SpriteGfxHandler *param0, u32 param1)
