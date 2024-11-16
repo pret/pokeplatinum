7dba186c17faf50cb2480d6f58e21827564456d4
Viperio19 <viperio19yt@gmail.com>
2024-11-16 14:04:51 +0100

    Use LEARNED_MOVES_MAX in MoveDisplayInfo struct definition

--- include/battle/move_display_info.h
+++ include/battle/move_display_info.h
@@ -1,10 +1,12 @@
 #ifndef POKEPLATINUM_MOVE_DISPLAY_INFO
 #define POKEPLATINUM_MOVE_DISPLAY_INFO
 
+#include "constants/moves.h"
+
 typedef struct MoveDisplayInfo {
-    u16 move[4];
-    u16 curPP[4];
-    u16 maxPP[4];
+    u16 move[LEARNED_MOVES_MAX];
+    u16 curPP[LEARNED_MOVES_MAX];
+    u16 maxPP[LEARNED_MOVES_MAX];
 } MoveDisplayInfo;
 
 #endif // POKEPLATINUM_MOVE_DISPLAY_INFO
