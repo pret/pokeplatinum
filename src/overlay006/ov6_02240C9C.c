4beee4a8dcb3e27574a4915b71eb1691ed5d5742
DylanDoesProgramming664 <120761889+DylanDoesProgramming664@users.noreply.github.com>
2025-01-18 00:16:24 -0500

    TRAINER_NAME_LEN location. Naming conventions for ov6_02240C9C.

--- src/overlay006/ov6_02240C9C.c
+++ src/overlay006/ov6_02240C9C.c
@@ -810,131 +810,131 @@ static BOOL ov6_022418DC(FieldSystem *fieldSystem, u32 encounterRate)
     }
 }
 
-static u8 ov6_02241904(void)
+static u8 GetGrassEncounterSlot(void)
 {
-    u8 v0 = inline_020564D0(100);
+    u8 roll = inline_020564D0(100); // a common rng function
 
-    if (v0 < 20) {
+    if (roll < 20) {
         return 0;
     }
 
-    if (v0 >= 20 && v0 < 40) {
+    if (roll >= 20 && roll < 40) {
         return 1;
     }
 
-    if (v0 >= 40 && v0 < 50) {
+    if (roll >= 40 && roll < 50) {
         return 2;
     }
 
-    if (v0 >= 50 && v0 < 60) {
+    if (roll >= 50 && roll < 60) {
         return 3;
     }
 
-    if (v0 >= 60 && v0 < 70) {
+    if (roll >= 60 && roll < 70) {
         return 4;
     }
 
-    if (v0 >= 70 && v0 < 80) {
+    if (roll >= 70 && roll < 80) {
         return 5;
     }
 
-    if (v0 >= 80 && v0 < 85) {
+    if (roll >= 80 && roll < 85) {
         return 6;
     }
 
-    if (v0 >= 85 && v0 < 90) {
+    if (roll >= 85 && roll < 90) {
         return 7;
     }
 
-    if (v0 >= 90 && v0 < 94) {
+    if (roll >= 90 && roll < 94) {
         return 8;
     }
 
-    if (v0 >= 94 && v0 < 98) {
+    if (roll >= 94 && roll < 98) {
         return 9;
     }
 
-    if (v0 == 98) {
+    if (roll == 98) {
         return 10;
     }
 
     return 11;
 }
 
-static u8 ov6_022419A0(void)
+static u8 GetWaterEncounterSlot(void)
 {
-    u8 v0 = inline_020564D0(100);
+    u8 roll = inline_020564D0(100); // a common rng function
 
-    if (v0 < 60) {
+    if (roll < 60) {
         return 0;
     }
 
-    if (v0 >= 60 && v0 < 90) {
+    if (roll >= 60 && roll < 90) {
         return 1;
     }
 
-    if (v0 >= 90 && v0 < 95) {
+    if (roll >= 90 && roll < 95) {
         return 2;
     }
 
-    if (v0 >= 95 && v0 < 99) {
+    if (roll >= 95 && roll < 99) {
         return 3;
     }
 
     return 4;
 }
 
-static u8 ov6_022419EC(const int fishingRodType)
+static u8 GetRodEncounterSlot(const int fishingRodType)
 {
-    u8 v1 = 0;
+    u8 encSlot = 0;
 
-    u8 v0 = inline_020564D0(100);
+    u8 roll = inline_020564D0(100); // a common rng function
 
     switch (fishingRodType) {
     case FISHING_TYPE_OLD_ROD:
-        if (v0 < 60) {
-            v1 = 0;
-        } else if (v0 < 90) {
-            v1 = 1;
-        } else if (v0 < 95) {
-            v1 = 2;
-        } else if (v0 < 99) {
-            v1 = 3;
+        if (roll < 60) {
+            encSlot = 0;
+        } else if (roll < 90) {
+            encSlot = 1;
+        } else if (roll < 95) {
+            encSlot = 2;
+        } else if (roll < 99) {
+            encSlot = 3;
         } else {
-            v1 = 4;
+            encSlot = 4;
         }
         break;
     case FISHING_TYPE_GOOD_ROD:
-        if (v0 < 40) {
-            v1 = 0;
-        } else if (v0 < 80) {
-            v1 = 1;
-        } else if (v0 < 95) {
-            v1 = 2;
-        } else if (v0 < 99) {
-            v1 = 3;
+        if (roll < 40) {
+            encSlot = 0;
+        } else if (roll < 80) {
+            encSlot = 1;
+        } else if (roll < 95) {
+            encSlot = 2;
+        } else if (roll < 99) {
+            encSlot = 3;
         } else {
-            v1 = 4;
+            encSlot = 4;
         }
         break;
     case FISHING_TYPE_SUPER_ROD:
-        if (v0 < 40) {
-            v1 = 0;
-        } else if (v0 < 80) {
-            v1 = 1;
-        } else if (v0 < 95) {
-            v1 = 2;
-        } else if (v0 < 99) {
-            v1 = 3;
+        if (roll < 40) {
+            encSlot = 0;
+        } else if (roll < 80) {
+            encSlot = 1;
+        } else if (roll < 95) {
+            encSlot = 2;
+        } else if (roll < 99) {
+            encSlot = 3;
         } else {
-            v1 = 4;
+            encSlot = 4;
         }
         break;
     default:
         GF_ASSERT(FALSE);
     }
 
-    return v1;
+    return encSlot;
 }
 
 static void ov6_02241A90(Pokemon *mon, u8 *encounterRate)
