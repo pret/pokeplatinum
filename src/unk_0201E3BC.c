8ecc26e07adb505997a7920ee43c5d59a9dbca8e
Gudf <28691694+Gudf@users.noreply.github.com>
2025-01-31 14:23:10 +0100

    `deltaX` to `dx`, `deltaY` to `dy`

--- src/unk_0201E3BC.c
+++ src/unk_0201E3BC.c
@@ -7,8 +7,8 @@
 u32 CalcDistance2D(s32 x0, s32 y0, s32 x1, s32 y1)
 {
 
-    s32 deltaX = x0 - x1;
-    s32 deltaY = y0 - y1;
+    s32 dx = x0 - x1;
+    s32 dy = y0 - y1;
 
-    return FX_Sqrt(((deltaX * deltaX) + (deltaY * deltaY)) << FX32_SHIFT) >> FX32_SHIFT;
+    return FX_Sqrt(((dx * dx) + (dy * dy)) << FX32_SHIFT) >> FX32_SHIFT;
 }
