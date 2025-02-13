9495c4fe40c62282a4f870c348fcda012fdc21e8
Lucie <callum.d.stew@gmail.com>
2025-02-13 17:11:11 +0000

    fixed header define

--- include/scrcmd_dummy_23F_242.h
+++ include/scrcmd_dummy_23F_242.h
@@ -1,5 +1,5 @@
-#ifndef POKEPLATINUM_DUMMY_SCRCMD_0204F02C_H
-#define POKEPLATINUM_DUMMY_SCRCMD_0204F02C_H
+#ifndef POKEPLATINUM_SCRCMD_DUMMY_23F_242_H
+#define POKEPLATINUM_SCRCMD_DUMMY_23F_242_H
 
 #include "field_script_context.h"
 
@@ -8,4 +8,4 @@ BOOL ScrCmd_Dummy240(ScriptContext *param0);
 BOOL ScrCmd_Dummy241(ScriptContext *param0);
 BOOL ScrCmd_Dummy242(ScriptContext *param0);
 
-#endif // POKEPLATINUM_DUMMY_SCRCMD_0204F02C_H
+#endif // POKEPLATINUM_SCRCMD_DUMMY_23F_242_H
