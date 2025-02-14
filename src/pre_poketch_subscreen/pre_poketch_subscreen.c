84d8f10920df3b2c29d49e688a3e672a23ff34e0
Lucie <callum.d.stew@gmail.com>
2025-02-14 20:29:02 +0000

    updated constants

--- src/pre_poketch_subscreen/pre_poketch_subscreen.c
+++ src/pre_poketch_subscreen/pre_poketch_subscreen.c
@@ -7,9 +7,6 @@
 #include "heap.h"
 #include "narc.h"
 
-#define PRE_POKETCH_DISPLAY_HEAP_SIZE    0x18000
-#define PRE_POKETCH_DISPLAY_NARC_HEAP_ID 8
-
 #define PRE_POKETCH_DISPLAY_NARC_TILES_IDX   10
 #define PRE_POKETCH_DISPLAY_NARC_TILEMAP_IDX 11
 #define PRE_POKETCH_DISPLAY_NARC_PALETTE_IDX 12
@@ -17,22 +14,21 @@
 void PrePoketchSubscreen_Init(BgConfig *bgConfig)
 {
     static const BgTemplate template = {
-        0,
-        0,
-        0x800,
-        0,
-        1,
-        GX_BG_COLORMODE_16,
-        GX_BG_SCRBASE_0x6000,
-        GX_BG_CHARBASE_0x00000,
-        GX_BG_EXTPLTT_01,
-        0,
-        0,
-        0,
-        0
+        .x = 0,
+        .y = 0,
+        .bufferSize = 0x800,
+        .baseTile = 0,
+        .screenSize = BG_SCREEN_SIZE_256x256,
+        .colorMode = GX_BG_COLORMODE_16,
+        .screenBase = GX_BG_SCRBASE_0x6000,
+        .charBase = GX_BG_CHARBASE_0x00000,
+        .bgExtPltt = GX_BG_EXTPLTT_01,
+        .priority = 0,
+        .areaOver = 0,
+        .mosaic = FALSE,
     };
 
-    Heap_Create(HEAP_ID_APPLICATION, HEAP_ID_POKETCH_APP, PRE_POKETCH_DISPLAY_HEAP_SIZE);
+    Heap_Create(HEAP_ID_APPLICATION, HEAP_ID_POKETCH_APP, HEAP_SIZE_PRE_POKETCH_SUBSCREEN);
 
     GXS_SetGraphicsMode(GX_BGMODE_0);
     GX_SetBankForSubBG(GX_VRAM_SUB_BG_32_H);
@@ -41,11 +37,11 @@ void PrePoketchSubscreen_Init(BgConfig *bgConfig)
 
     Bg_InitFromTemplate(bgConfig, BG_LAYER_SUB_0, &template, BG_TYPE_STATIC);
 
-    NARC *narc = NARC_ctor(NARC_INDEX_GRAPHIC__POKETCH, PRE_POKETCH_DISPLAY_NARC_HEAP_ID);
+    NARC *narc = NARC_ctor(NARC_INDEX_GRAPHIC__POKETCH, HEAP_ID_POKETCH_APP);
 
-    Graphics_LoadTilesToBgLayerFromOpenNARC(narc, PRE_POKETCH_DISPLAY_NARC_TILES_IDX, bgConfig, BG_LAYER_SUB_0, 0, 0, TRUE, PRE_POKETCH_DISPLAY_NARC_HEAP_ID);
-    Graphics_LoadTilemapToBgLayerFromOpenNARC(narc, PRE_POKETCH_DISPLAY_NARC_TILEMAP_IDX, bgConfig, BG_LAYER_SUB_0, 0, 0, TRUE, PRE_POKETCH_DISPLAY_NARC_HEAP_ID);
-    Graphics_LoadPaletteFromOpenNARC(narc, PRE_POKETCH_DISPLAY_NARC_PALETTE_IDX, PAL_LOAD_SUB_BG, 0, 0x20, PRE_POKETCH_DISPLAY_NARC_HEAP_ID);
+    Graphics_LoadTilesToBgLayerFromOpenNARC(narc, PRE_POKETCH_DISPLAY_NARC_TILES_IDX, bgConfig, BG_LAYER_SUB_0, 0, 0, TRUE, HEAP_ID_POKETCH_APP);
+    Graphics_LoadTilemapToBgLayerFromOpenNARC(narc, PRE_POKETCH_DISPLAY_NARC_TILEMAP_IDX, bgConfig, BG_LAYER_SUB_0, 0, 0, TRUE, HEAP_ID_POKETCH_APP);
+    Graphics_LoadPaletteFromOpenNARC(narc, PRE_POKETCH_DISPLAY_NARC_PALETTE_IDX, PAL_LOAD_SUB_BG, 0, 0x20, HEAP_ID_POKETCH_APP);
     NARC_dtor(narc);
 
     GXS_SetVisibleWnd(GX_WNDMASK_NONE);
@@ -55,7 +51,7 @@ void PrePoketchSubscreen_Init(BgConfig *bgConfig)
 void PrePoketchSubscreen_Exit(BgConfig *bgConfig)
 {
     Bg_FreeTilemapBuffer(bgConfig, BG_LAYER_SUB_0);
-    Heap_Destroy(PRE_POKETCH_DISPLAY_NARC_HEAP_ID);
+    Heap_Destroy(HEAP_ID_POKETCH_APP);
 }
 
 BOOL PrePoketchSubscreen_IsDone(BgConfig *bgConfig)
