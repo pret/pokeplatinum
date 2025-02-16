029f8f48e0f279f1a892075d2e10bc52bb0898a9
Lucie <callum.d.stew@gmail.com>
2025-02-16 01:19:15 +0000

    updated header file

--- include/overlay025/ov25_02254560.h
+++ include/overlay025/ov25_02254560.h
@@ -13,9 +13,9 @@
 BOOL ov25_02254560(UnkStruct_ov25_02254560 **param0, const UnkStruct_ov25_02254560_1 *param1, NNSG2dOamManagerInstance *param2, PoketchSystem *poketchSys);
 UnkStruct_ov25_022555E8 *ov25_02254664(void);
 BgConfig *Poketch_GetBgConfig(void);
-void Poketch_LoadActivePalette(u32 param0, u32 param1);
-void Poketch_LoadActiveBacklightPalette(u32 param0, u32 param1);
-void Poketch_CopuActivePalette(u16 *param0);
+void Poketch_LoadActivePalette(u32 bgOffset, u32 objOffset);
+void Poketch_LoadActiveBacklightPalette(u32 unused_1, u32 unused_2);
+void Poketch_CopyActivePalette(u16 *dest);
 void ov25_02254754(UnkStruct_ov25_02254560 *param0);
 void ov25_022547D0(UnkStruct_ov25_02254560 *param0, u32 param1);
 BOOL ov25_022547F4(UnkStruct_ov25_02254560 *param0, u32 param1);
