53db391b60d061cdbeb1cf21a0b1cb532ffc937e
mudskipper13 <MariaJumpMan001@hotmail.com>
2025-02-19 22:32:47 +0700

    ov7_0224CD28/shop menu: partial UnkStruct_ov7_0224D008 -> ShopMenu

--- include/overlay007/ov7_0224CD28.h
+++ include/overlay007/ov7_0224CD28.h
@@ -12,6 +12,19 @@ enum MartTypes {
     MART_TYPE_FRONTIER, // uses Battle Points instead of normal money
 };
 
+enum ShopStaticWindow {
+    SHOP_WINDOW_ITEM_LIST = 0,
+    SHOP_WINDOW_ITEM_DESCRIPTION,
+    SHOP_WINDOW_CURRENT_MONEY,
+    SHOP_WINDOW_QUANTITY_TOTAL_PRICE,
+    SHOP_WINDOW_ITEMS_IN_BAG,
+    SHOP_WINDOW_MESSAGE,
+
+    SHOP_WINDOW_MAX
+};
+
+#define MAX_ITEM_SHOWN 7
+
 void Shop_Start(FieldTask *task, FieldSystem *fieldSystem, u16 *shopItems, u8 martType, BOOL incDeptStoreBuyCount);
 BOOL ov7_0224CEAC(FieldTask *param0);
 
