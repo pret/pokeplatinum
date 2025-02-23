b37e5caf9a22da6840ae2e288fe29a10dcea9ea0
RavePossum <ravepossum@proton.me>
2025-02-23 09:19:58 -0500

    Move PokemonSpriteShadow to pokemon sprite struct def header

--- include/struct_defs/pokemon_sprite.h
+++ include/struct_defs/pokemon_sprite.h
@@ -7,7 +7,6 @@
 
 #include "struct_defs/sprite_animation_frame.h"
 #include "struct_defs/struct_02007C7C_sub1.h"
-#include "struct_defs/struct_02007C7C_sub2.h"
 
 typedef struct PokemonSprite PokemonSprite;
 
@@ -23,6 +22,21 @@ typedef struct PokemonSpriteTemplate {
 
     u32 personality; //< Cached personality value for Pokemon front-sprites. Specifically used for Spinda spots.
 } PokemonSpriteTemplate;
+
+typedef struct PokemonSpriteShadow {
+    u16 plttSlot : 2;
+    u16 shouldAdjustX : 1;
+    u16 shouldAdjustY : 1;
+    u16 isAffine : 1;
+    u16 size : 2;
+    u16 padding_00 : 9;
+    s8 height;
+    u8 padding_03;
+    s16 x;
+    s16 y;
+    s16 xOffset;
+    s16 yOffset;
+} PokemonSpriteShadow;
 
 struct PokemonSprite {
     u32 unk_00_0 : 1;
