dd7046ff07c62ca11afcff96e6820c3de509239a
RavePossum <ravepossum@proton.me>
2025-02-23 11:41:40 -0500

    Move PokemonSpriteTransforms to pokemon sprite struct def header

--- include/struct_defs/pokemon_sprite.h
+++ include/struct_defs/pokemon_sprite.h
@@ -6,9 +6,9 @@
 #include "constants/heap.h"
 
 #include "struct_defs/sprite_animation_frame.h"
-#include "struct_defs/struct_02007C7C_sub1.h"
 
 typedef struct PokemonSprite PokemonSprite;
+typedef struct PokemonSpriteTransforms PokemonSpriteTransforms;
 
 typedef void(PokemonSpriteCallback)(PokemonSprite *, PokemonSpriteTransforms *);
 
@@ -22,6 +22,49 @@ typedef struct PokemonSpriteTemplate {
 
     u32 personality; //< Cached personality value for Pokemon front-sprites. Specifically used for Spinda spots.
 } PokemonSpriteTemplate;
+
+struct PokemonSpriteTransforms {
+    s16 xCenter;
+    s16 yCenter;
+    int zCenter;
+    s16 xOffset;
+    s16 yOffset;
+    int zOffset;
+    s16 affineWidth;
+    s16 affineHeight;
+    u16 rotationX;
+    u16 rotationY;
+    u16 rotationZ;
+    u16 padding_1A;
+    s16 xPivot;
+    s16 yPivot;
+    u8 xOffset2;
+    u8 yOffset2;
+    u8 width;
+    u8 height;
+    u8 fadeCurrent;
+    u8 fadeEnd;
+    u8 fadeDelayCounter;
+    u8 fadeDelayLength;
+    u32 fadeTargetColor;
+    u32 diffuseR : 5;
+    u32 diffuseG : 5;
+    u32 diffuseB : 5;
+    u32 ambientR : 5;
+    u32 ambientG : 5;
+    u32 ambientB : 5;
+    u32 padding_2C_30 : 2;
+    u32 hasVanished : 1;
+    u32 visible : 1;
+    u32 alpha : 5;
+    u32 padding_30_07 : 2;
+    u32 flipH : 1;
+    u32 flipV : 1;
+    u32 dontDraw : 1;
+    u32 fadeActive : 1;
+    u32 mosaicIntensity : 4;
+    u32 padding_30_17 : 15;
+};
 
 typedef struct PokemonSpriteShadow {
     u16 plttSlot : 2;
