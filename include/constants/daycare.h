6eda6260507288c9db505ab7218f995a518a2c09
Eduardo Quezada <eduardo602002@gmail.com>
2025-03-16 16:44:01 -0300

    Format

--- include/constants/daycare.h
+++ include/constants/daycare.h
@@ -2,6 +2,6 @@
 #define POKEPLATINUM_CONSTANTS_DAYCARE_H
 
 #define DAYCARE_MON_COUNT 2
-#define EGG_GENDER_MALE 0x8000  // used to create a male egg from a female-only parent species (e.g. Nidoran)
+#define EGG_GENDER_MALE   0x8000 // used to create a male egg from a female-only parent species (e.g. Nidoran)
 
 #endif // POKEPLATINUM_CONSTANTS_DAYCARE_H
