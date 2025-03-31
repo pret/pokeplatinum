1083f5d6c58651034404be6f50c4558d5ec00a2c
Rachel <lhearachel@proton.me>
2025-03-31 11:11:09 -0700

    Apply formatting rules to #451

--- src/scrcmd_game_corner_prize.c
+++ src/scrcmd_game_corner_prize.c
@@ -20,25 +20,25 @@ BOOL ScrCmd_GetGameCornerPrizeData(ScriptContext *ctx)
     u16 *price = ScriptContext_GetVarPointer(ctx);
 
     static const GameCornerPrize sGameCornerPrizeData[] = {
-        { ITEM_SILK_SCARF,      1000 },
-        { ITEM_WIDE_LENS,       1000 },
-        { ITEM_ZOOM_LENS,       1000 },
-        { ITEM_METRONOME,       1000 },
-        { ITEM_TM90,            2000 },
-        { ITEM_TM58,            2000 },
-        { ITEM_TM75,            4000 },
-        { ITEM_TM32,            4000 },
-        { ITEM_TM44,            6000 },
-        { ITEM_TM89,            6000 },
-        { ITEM_TM10,            6000 },
-        { ITEM_TM27,            8000 },
-        { ITEM_TM21,            8000 },
-        { ITEM_TM35,            10000 },
-        { ITEM_TM24,            10000 },
-        { ITEM_TM13,            10000 },
-        { ITEM_TM29,            10000 },
-        { ITEM_TM74,            15000 },
-        { ITEM_TM68,            20000 }
+        { ITEM_SILK_SCARF, 1000 },
+        { ITEM_WIDE_LENS, 1000 },
+        { ITEM_ZOOM_LENS, 1000 },
+        { ITEM_METRONOME, 1000 },
+        { ITEM_TM90, 2000 },
+        { ITEM_TM58, 2000 },
+        { ITEM_TM75, 4000 },
+        { ITEM_TM32, 4000 },
+        { ITEM_TM44, 6000 },
+        { ITEM_TM89, 6000 },
+        { ITEM_TM10, 6000 },
+        { ITEM_TM27, 8000 },
+        { ITEM_TM21, 8000 },
+        { ITEM_TM35, 10000 },
+        { ITEM_TM24, 10000 },
+        { ITEM_TM13, 10000 },
+        { ITEM_TM29, 10000 },
+        { ITEM_TM74, 15000 },
+        { ITEM_TM68, 20000 }
     };
 
     *item = sGameCornerPrizeData[index].item;
