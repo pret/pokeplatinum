61dd73c53af0511ed6d47236f4b165cb4a8c9f3f
Gudf <28691694+Gudf@users.noreply.github.com>
2025-04-04 20:45:15 +0200

    Document `unk_0204B830` as `scrcmd_mystery_gift`

---
+++ include/scrcmd_mystery_gift.h
@@ -0,0 +1,8 @@
+#ifndef POKEPLATINUM_SCRCMD_MYSTERY_GIFT_H
+#define POKEPLATINUM_SCRCMD_MYSTERY_GIFT_H
+
+#include "field_script_context.h"
+
+BOOL ScrCmd_MysteryGiftGive(ScriptContext *param0);
+
+#endif // POKEPLATINUM_SCRCMD_MYSTERY_GIFT_H
