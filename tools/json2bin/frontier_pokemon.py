4991ee1fe09de804e0759ac227d09b84b1e7a9a7
tillgeissler <geisslertill@web.de>
2025-05-31 20:03:33 +0200

    Refactor frontier pokemon structure

---
+++ tools/json2bin/frontier_pokemon.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import json
+import pathlib
+import sys
+
+from convert import from_item, from_move, from_nature, from_species, u16, u8
+
+def parse_moves(moves: list[str]) -> bytes:
+    return b"".join(
+        [u16(from_move(move)) for move in moves]
+    )
+
+def parse_ev_flags(ev_flags) -> bytes:
+    value = 0
+    for i, flag in enumerate(("hp", "attack", "defense", "speed", "special_attack", "special_defense")):
+        if ev_flags[flag]:
+            value += 1 << i
+    return u8(value)
+
+input_path = pathlib.Path(sys.argv[1])
+output_path = pathlib.Path(sys.argv[2])
+
+try:
+    with open(input_path, 'r', encoding='utf-8') as input_file:
+        data = json.load(input_file)
+except json.decoder.JSONDecodeError as e:
+    print(e)
+    sys.exit(1)
+
+packables = bytearray([])
+packables.extend(u16(from_species(data['species'])))
+packables.extend(parse_moves(data['moves']))
+packables.extend(parse_ev_flags(data['evFlags']))
+packables.extend(u8(from_nature(data['nature'])))
+packables.extend(u16(from_item(data['item'])))
+packables.extend(u16(data['form']))
+
+
+with open(output_path, 'wb') as output_file:
+    output_file.write(packables)
