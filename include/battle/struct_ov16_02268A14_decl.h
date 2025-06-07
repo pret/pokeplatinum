ee962db3a1ea372142b0b349146dda378f43ef77
Yako <yakoswg@gmail.com>
2025-06-07 12:09:25 +0200

    Revert naming of UnkStruct_ov16_02268A14

--- include/battle/struct_ov16_02268A14_decl.h
+++ include/battle/struct_ov16_02268A14_decl.h
@@ -1,6 +1,6 @@
 #ifndef POKEPLATINUM_STRUCT_OV16_02268A14_DECL_H
 #define POKEPLATINUM_STRUCT_OV16_02268A14_DECL_H
 
-typedef struct BattleDisplayData UnkStruct_ov16_02268A14;
+typedef struct UnkStruct_ov16_02268A14_t UnkStruct_ov16_02268A14;
 
 #endif // POKEPLATINUM_STRUCT_OV16_02268A14_DECL_H
