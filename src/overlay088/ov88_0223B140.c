7721ebb259bf2d88f9872067313259603de164fb
ellieplayswow <164806095+ellieplayswow@users.noreply.github.com>
2025-06-29 21:30:46 +0100

    document PalPad (#580)

--- src/overlay088/ov88_0223B140.c
+++ src/overlay088/ov88_0223B140.c
@@ -9,7 +9,6 @@
 #include "struct_decls/struct_0202B370_decl.h"
 #include "struct_decls/struct_02095E80_decl.h"
 #include "struct_defs/chatot_cry.h"
-#include "struct_defs/struct_02027F8C.h"
 #include "struct_defs/struct_02095E80_t.h"
 #include "struct_defs/struct_02099F80.h"
 
@@ -41,6 +40,7 @@
 #include "message_util.h"
 #include "narc.h"
 #include "overlay_manager.h"
+#include "pal_pad.h"
 #include "party.h"
 #include "pltt_transfer.h"
 #include "pokemon.h"
@@ -1647,20 +1647,21 @@ static void ov88_0223D0C0(SaveData *saveData)
 
 static void ov88_0223D0D4(TrainerInfo *param0, PalPad *param1, PalPad *param2)
 {
+    // this is the exact same as PalPad_CreateNetworkObject ?
     int v0;
 
-    CharCode_Copy(param2->unk_00, TrainerInfo_Name(param0));
+    CharCode_Copy(param2->trainerName, TrainerInfo_Name(param0));
 
-    param2->unk_10 = TrainerInfo_ID(param0);
-    param2->unk_14 = TrainerInfo_RegionCode(param0);
-    param2->unk_15 = TrainerInfo_GameCode(param0);
-    param2->unk_16 = TrainerInfo_Gender(param0);
+    param2->trainerId = TrainerInfo_ID(param0);
+    param2->regionCode = TrainerInfo_RegionCode(param0);
+    param2->gameCode = TrainerInfo_GameCode(param0);
+    param2->gender = TrainerInfo_Gender(param0);
 
     for (v0 = 0; v0 < 16; v0++) {
-        param2->unk_18[v0] = param1[v0].unk_10;
-        param2->unk_58[v0] = param1[v0].unk_15;
-        param2->unk_68[v0] = param1[v0].unk_14;
-        param2->unk_78[v0] = param1[v0].unk_16;
+        param2->associatedTrainerIds[v0] = param1[v0].trainerId;
+        param2->associatedTrainerGameCodes[v0] = param1[v0].gameCode;
+        param2->associatedTrainerRegionCodes[v0] = param1[v0].regionCode;
+        param2->associatedTrainerGenders[v0] = param1[v0].gender;
     }
 
     CommSys_SendDataHuge(28, param2, sizeof(PalPad));
