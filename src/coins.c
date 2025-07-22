141384ea2995ae31d9ea1b1865b5a9555b5c71d0
Eduardo Quezada <eduardo602002@gmail.com>
2025-07-21 21:03:35 -0400

    format

--- src/coins.c
+++ src/coins.c
@@ -3,20 +3,24 @@
 #include <nitro.h>
 #include <string.h>
 
-void Coins_Init(u16 *coins) {
+void Coins_Init(u16 *coins)
+{
     *coins = 0;
 }
 
-void Coins_SetValue(u16 *coins, u32 value) {
+void Coins_SetValue(u16 *coins, u32 value)
+{
     GF_ASSERT(value <= MAX_COINS);
     *coins = value;
 }
 
-u16 Coins_GetValue(u16 *coins) {
+u16 Coins_GetValue(u16 *coins)
+{
     return *coins;
 }
 
-BOOL Coins_Add(u16 *coins, u16 amount) {
+BOOL Coins_Add(u16 *coins, u16 amount)
+{
     if (*coins >= MAX_COINS) {
         return FALSE;
     }
@@ -30,11 +34,13 @@ BOOL Coins_Add(u16 *coins, u16 amount) {
     return TRUE;
 }
 
-BOOL Coins_CanAdd(u16 *coins, u16 amount) {
+BOOL Coins_CanAdd(u16 *coins, u16 amount)
+{
     return (u32)(amount + *coins) <= MAX_COINS;
 }
 
-BOOL Coins_Subtract(u16 *coins, u16 amount) {
+BOOL Coins_Subtract(u16 *coins, u16 amount)
+{
     if (*coins < amount) {
         return FALSE;
     }
