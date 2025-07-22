d87af820e320e9490730cc35cfe33d95c9482894
Fexty12573 <bogofogo1@gmail.com>
2025-07-22 09:35:06 +0200

    Document confusion script func

--- include/overlay012/ov12_0222F700.h
+++ include/overlay012/ov12_0222F700.h
@@ -8,7 +8,7 @@
 void BattleAnimScriptFunc_QuickAttack(BattleAnimSystem *param0);
 void BattleAnimScriptFunc_DrillPeck(BattleAnimSystem *param0);
 void ov12_0222FE30(BattleAnimSystem *param0, SpriteSystem *param1, SpriteManager *param2, ManagedSprite *param3);
-void ov12_022300C4(BattleAnimSystem *param0);
+void BattleAnimScriptFunc_Confusion(BattleAnimSystem *param0);
 void ov12_022303D0(BattleAnimSystem *param0);
 void ov12_02230754(BattleAnimSystem *param0);
 void ov12_02230804(BattleAnimSystem *param0);
