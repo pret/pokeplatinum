70ca0698239c644b2b6466ee38e868ace7a782ec
Eduardo Quezada <eduardo602002@gmail.com>
2025-08-05 13:25:04 -0400

    pt2

--- include/struct_defs/struct_02090800.h
+++ include/struct_defs/struct_02090800.h
@@ -1,8 +1,6 @@
 #ifndef POKEPLATINUM_STRUCT_02090800_H
 #define POKEPLATINUM_STRUCT_02090800_H
 
-#include "struct_defs/struct_02090800_sub1.h"
-
 #include "message.h"
 #include "pokemon.h"
 #include "string_template.h"
