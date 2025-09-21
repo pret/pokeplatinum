e2fcab79a926401d4faa687c19078c51cdfd69da
innocenthedgehog <56793981+innocenthedgehog@users.noreply.github.com>
2025-09-21 01:40:01 +0100

    Document map scripts for Sandgem Mart (#776)

--- res/field/scripts/scripts_survival_area_mart.s
+++ res/field/scripts/scripts_survival_area_mart.s
@@ -12,7 +12,7 @@ _0012:
     PlayFanfare SEQ_SE_CONFIRM
     LockAll
     FacePlayer
-    CallCommonScript 0x7E3
+    GenericVendorGreeting
     CloseMessageWithoutErasing
     PokeMartCommon
     ReleaseAll
