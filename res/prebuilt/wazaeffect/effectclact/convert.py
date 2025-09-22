2b7bb44f064a9a3c4f9b4e1f07388fd2d7dd723d
Fexty12573 <bogofogo1@gmail.com>
2025-09-22 22:32:36 +0200

    update effectclact conversion script

--- res/prebuilt/wazaeffect/effectclact/convert.py
+++ res/prebuilt/wazaeffect/effectclact/convert.py
@@ -10,6 +10,34 @@ DIRS = [
     (Path("wepltt.narc.d"), Path("wepltt.conv"))
 ]
 
+CELL_ARGS = {
+    "00002.NCGR": [],
+    "00008.NCGR": ["-nosnap", "-noskip"],
+}
+
+FIXED_PALETTES = {
+    "00016.NCGR": "00015.NCLR",
+    "00017.NCGR": "00016.NCLR",
+    "00018.NCGR": "00017.NCLR",
+    "00019.NCGR": "00018.NCLR",
+    "00020.NCGR": "00019.NCLR",
+    "00021.NCGR": "00020.NCLR",
+    "00022.NCGR": "00021.NCLR",
+    "00023.NCGR": "00022.NCLR",
+    "00024.NCGR": "00023.NCLR",
+    "00025.NCGR": "00024.NCLR",
+    "00026.NCGR": "00025.NCLR",
+    "00027.NCGR": "00026.NCLR",
+    "00028.NCGR": "00027.NCLR",
+    "00029.NCGR": "00028.NCLR",
+}
+
+def get_cell_args(filename: str) -> list[str]:
+    return CELL_ARGS.get(filename, ["-nosnap", "-noskip"])
+
+def get_fixed_palette(filename: str) -> Path:
+    return DIRS[3][0] / FIXED_PALETTES.get(filename, filename.replace("NCGR", "NCLR"))
+
 for file in DIRS[0][0].glob("*.NCER.lz"):
     intermediate_file = DIRS[0][1] / file.name.replace("NCER.lz", "NCER")
     output_file = intermediate_file.with_suffix(".json")
@@ -22,7 +50,7 @@ for file in DIRS[1][0].glob("*.NANR.lz"):
     output_file = intermediate_file.with_suffix(".json")
     subprocess.run(["lnitrogfx", file, intermediate_file], check=True)
     subprocess.run(["lnitrogfx", intermediate_file, output_file], check=True)
-    intermediate_file.unlink()
+    # intermediate_file.unlink()
 
 for file in DIRS[2][0].glob("*.NCGR.lz"):
     intermediate_file = DIRS[2][1] / file.name.replace("NCGR.lz", "NCGR")
@@ -35,13 +63,17 @@ for file in DIRS[2][0].glob("*.NCGR.lz"):
         intermediate_file,
         output_file,
         "-palette",
-        palette,
+        get_fixed_palette(intermediate_file.name),
         "-cell",
         cell,
-        "-nosnap", "-noskip"
+        *get_cell_args(intermediate_file.name),
         ], check=True)
-    intermediate_file.unlink()
+    # intermediate_file.unlink()
+
+for file in DIRS[3][0].glob("*.NCLR"):
+    output_file = DIRS[3][1] / file.name.replace("NCLR", "pal")
+    subprocess.run(["lnitrogfx", file, output_file], check=True)
 
 # Remove intermediate cell files
-for file in DIRS[0][1].glob("*.NCER"):
-    file.unlink()
+# for file in DIRS[0][1].glob("*.NCER"):
+#     file.unlink()
