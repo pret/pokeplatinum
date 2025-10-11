24ed52d82facbdf01c5f0e4ffea7f9deb0b8b6fb
VicSevenT <vicsevenT@proton.me>
2025-10-11 16:02:41 -0500

    Document Poketch Marking Map (Overlay 47) (#797)

---
+++ include/applications/poketch/poketch_map.h
@@ -0,0 +1,10 @@
+#ifndef POKEPLATINUM_POKETCH_MAP_H
+#define POKEPLATINUM_POKETCH_MAP_H
+
+#include "generated/maps.h"
+
+void PoketchMap_GetPlayerCursorPosition(u32 playerX, u32 playerY, u32 *mapX, u32 *mapY);
+void PoketchMap_GetHiddenLocationPosition(int hiddenLocation, u32 *x, u32 *y);
+BOOL PoketchMap_GetPositionFromMapID(enum MapID mapID, u32 *x, u32 *y);
+
+#endif // POKEPLATINUM_POKETCH_MAP_H
