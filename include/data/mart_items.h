b307a864ce851d75f451837f487769cdfd3b887f
Gudf <28691694+Gudf@users.noreply.github.com>
2025-12-15 06:08:07 +0100

    Unpack trainer ball capsules (#855)

--- include/data/mart_items.h
+++ include/data/mart_items.h
@@ -1,10 +1,10 @@
 #include "constants/goods.h"
 #include "constants/items.h"
-#include "constants/seals.h"
 #include "generated/mart_decor_id.h"
 #include "generated/mart_frontier_id.h"
 #include "generated/mart_seal_id.h"
 #include "generated/mart_specialties_id.h"
+#include "generated/seals.h"
 
 typedef struct {
     u16 itemID;
