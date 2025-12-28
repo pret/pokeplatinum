df3b5c18f4dc09ef912053f6bc6d8acc615aa20c
Eduardo Quezada <eduardo602002@gmail.com>
2025-12-28 00:37:44 -0300

    BoxPokemon_RestorePP

--- src/pokemon.c
+++ src/pokemon.c
@@ -4337,15 +4337,12 @@ void Pokemon_ClearBallCapsuleData(Pokemon *mon)
 void BoxPokemon_RestorePP(BoxPokemon *boxMon)
 {
     BOOL reencrypt = BoxPokemon_UnlockEncryption(boxMon);
-
     for (int i = 0; i < MAX_MON_MOVES; i++) {
-        if (BoxPokemon_GetData(boxMon, MON_DATA_MOVE1 + i, NULL)) {
-            u8 moveMaxPP = BoxPokemon_GetData(boxMon, MON_DATA_MOVE1_MAX_PP + i, NULL);
-
-            BoxPokemon_SetData(boxMon, MON_DATA_MOVE1_PP + i, &moveMaxPP);
+        if (BoxPokemon_GetData(boxMon, MON_DATA_MOVE1 + i, NULL) != MOVE_NONE) {
+            u8 pp = BoxPokemon_GetData(boxMon, MON_DATA_MOVE1_MAX_PP + i, NULL);
+            BoxPokemon_SetData(boxMon, MON_DATA_MOVE1_PP + i, &pp);
         }
     }
-
     BoxPokemon_LockEncryption(boxMon, reencrypt);
 }
 
