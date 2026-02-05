02a154cb1e8d4584e2d269b8be5f4f01a09e7648
VicSevenT <vicsevenT@proton.me>
2026-02-05 17:35:23 -0600

    Document the Party Menu Form Change animation (Overlay 118)

--- include/overlay118/ov118_021D0D80.h
+++
@@ -1,9 +0,0 @@
-#ifndef POKEPLATINUM_OV118_021D0D80_H
-#define POKEPLATINUM_OV118_021D0D80_H
-
-#include "applications/party_menu/defs.h"
-
-void ov118_021D0D80(PartyMenuApplication *param0);
-int ov118_021D0DBC(PartyMenuApplication *param0);
-
-#endif // POKEPLATINUM_OV118_021D0D80_H
