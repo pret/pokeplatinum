3452e792f4008b084595ea390c9669ed6c911502
Viperio19 <viperio19yt@gmail.com>
2026-04-01 08:45:18 +0200

    Fix labels for blocks setting iron ruins warp

--- res/field/scripts/scripts_iron_island_b3f.s
+++ res/field/scripts/scripts_iron_island_b3f.s
@@ -9,23 +9,23 @@
 
 IronIslandB3F_OnLoad:
     CheckPartyHasFatefulEncounterRegigigas VAR_MAP_LOCAL_4
-    GoToIfEq VAR_MAP_LOCAL_4, FALSE, IronIslandB3F_SetWarpEventIronIslandIronRuins
-    GoToIfEq VAR_MAP_LOCAL_4, TRUE, IronIslandB3F_SetWarpEventIronRuins
+    GoToIfEq VAR_MAP_LOCAL_4, FALSE, IronIslandB3F_RemoveWarpIronRuinsWithRegisteel
+    GoToIfEq VAR_MAP_LOCAL_4, TRUE, IronIslandB3F_RemoveWarpIronRuinsWithoutRegisteel
     End
 
 IronIslandB3F_OnTransition:
     InitPersistedMapFeaturesForPlatformLift
     CallIfNe VAR_IRON_RUINS_STATE, RUINS_STATE_CAUGHT_REGI, IronIslandB3F_ResetIronRuinsState
     CheckPartyHasFatefulEncounterRegigigas VAR_MAP_LOCAL_4
-    GoToIfEq VAR_MAP_LOCAL_4, 0, IronIslandB3F_SetWarpEventIronIslandIronRuins
-    GoToIfEq VAR_MAP_LOCAL_4, 1, IronIslandB3F_SetWarpEventIronRuins
+    GoToIfEq VAR_MAP_LOCAL_4, FALSE, IronIslandB3F_RemoveWarpIronRuinsWithRegisteel
+    GoToIfEq VAR_MAP_LOCAL_4, TRUE, IronIslandB3F_RemoveWarpIronRuinsWithoutRegisteel
     End
 
-IronIslandB3F_SetWarpEventIronIslandIronRuins:
+IronIslandB3F_RemoveWarpIronRuinsWithRegisteel:
     SetWarpEventPos 3, 17, 1
     End
 
-IronIslandB3F_SetWarpEventIronRuins:
+IronIslandB3F_RemoveWarpIronRuinsWithoutRegisteel:
     SetWarpEventPos 2, 17, 1
     End
 
