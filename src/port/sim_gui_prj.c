988f9c32e1598aa4a6d232c9cbc661507ae20ab9
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-06-20 11:13:54 -0500

    Simplify some GUI code

--- src/port/sim_gui_prj.c
+++ src/port/sim_gui_prj.c
@@ -3,6 +3,7 @@
 #include "simulator/gui.h"
 #include <stdbool.h>
 
+#include <simulator/gui.h>
 #include "sim_gui_prj.h"
 
 static ImVec2 s_btnSize = {50, 20};
@@ -11,17 +12,6 @@ static bool s_createMonOpen;
 
 void SIM_GUI_Prj_main(bool * p_open) {
     igBegin("pokeplatinum", p_open, 0);
-    if(igButton("Create Pokemon", s_btnSize)) {
-        if(s_createMonOpen) {
-            s_createMonOpen = false;
-        } else {
-            GUI_CreateMon_Init();
-            s_createMonOpen = true;
-        }
-    }
-
-    if(s_createMonOpen) {
-        GUI_CreateMon_Main(&s_createMonOpen);
-    }
+    SIM_GUI_AppButton("Create Pokemon", &s_createMonOpen, GUI_CreateMon_Main);
     igEnd();
 }
