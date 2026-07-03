c58ebb19fa9a0dda182e4dd96ab7226af27ed9cc
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-07-03 10:45:05 -0500

    build: copy ROM header to header.bin instead of main.srl

--- tools/scripts/copy_pc_files.sh
+++ tools/scripts/copy_pc_files.sh
@@ -13,7 +13,7 @@ cat $2 | while read line; do
     cp -u $BUILDDIR/$SourceFile $OUTPUTDIR$DestFile
   fi
 done
-cp -u $BUILDDIR/../res/pc_port/header.bin $OUTPUTDIR/main.srl
+cp -u $BUILDDIR/../res/pc_port/header.bin $OUTPUTDIR/header.bin
 cp -u $BUILDDIR/../res/pc_port/dwc_nv.bin $OUTPUTDIR/firmware.bin
 cp $BUILDDIR/main $OUTPUTDIR/main
 
