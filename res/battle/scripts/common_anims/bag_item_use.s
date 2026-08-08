2b7f5fb8a52029eb63b31e425a6456a545778ba5
h2o-ds <h2o.daniel.dj@gmail.com>
2026-08-07 23:08:17 -0400

    battle scripts

--- res/battle/scripts/common_anims/bag_item_use.s
+++ res/battle/scripts/common_anims/bag_item_use.s
@@ -4,7 +4,7 @@
 
 L_0:
     LoadParticleResource 0, 28
-    PlayPannedSoundEffect SEQ_SE_DP_BT_001, BATTLE_SOUND_PAN_LEFT
+    PlayPannedSoundEffect SEQ_SE_DP_BT_001_sseq, BATTLE_SOUND_PAN_LEFT
     CreateEmitter 0, 1, EMITTER_CB_SET_POS_TO_ATTACKER
     WaitForAllEmitters
     UnloadParticleSystem 0
