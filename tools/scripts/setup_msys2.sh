97f2dc57e43afb262c99ee83df97a45b6e661f85
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-08-23 14:43:53 -0500

    libntr build fixes

--- tools/scripts/setup_msys2.sh
+++ tools/scripts/setup_msys2.sh
@@ -2,5 +2,5 @@
 
 echo Setting up MSys2 environment!
 
-pacman -S --noconfirm make p7zip git python3 flex bison ninja mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-SDL2 mingw-w64-ucrt-x86_64-nasm mingw-w64-ucrt-x86_64-enet mingw-w64-ucrt-x86_64-arm-none-eabi-gcc zip
+pacman -S --noconfirm make p7zip git python3 flex bison ninja mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-SDL2 mingw-w64-ucrt-x86_64-nasm mingw-w64-ucrt-x86_64-enet mingw-w64-ucrt-x86_64-arm-none-eabi-gcc zip mingw-w64-ucrt-x86_64-libpng
 git config --global core.autocrlf true
