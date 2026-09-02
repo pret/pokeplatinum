7553e85525302e231f84a4fd9ae4ccfd792a971b
CharlesFolz4 <CharlesFolz4@gmail.com>
2026-09-01 21:28:11 -0400

    WIP document weather

--- include/overlay005/ov5_021D5EB8.h
+++ include/overlay005/ov5_021D5EB8.h
@@ -3,16 +3,11 @@
 
 #include "field/field_system_decl.h"
 #include "overlay005/struct_ov5_021D5EF8_decl.h"
-#include "overlay005/struct_ov5_021D6594_decl.h"
 
 UnkStruct_ov5_021D5EF8 *ov5_021D5EB8(FieldSystem *fieldSystem);
 void ov5_021D5EF8(UnkStruct_ov5_021D5EF8 *param0);
-void ov5_021D5F24(UnkStruct_ov5_021D5EF8 *param0, int param1);
-BOOL ov5_021D5F7C(UnkStruct_ov5_021D5EF8 *param0, int param1);
-u32 ov5_021D5FE0(UnkStruct_ov5_021D5EF8 *param0);
-UnkStruct_ov5_021D6594 *ov5_021D6364(FieldSystem *fieldSystem);
-void ov5_021D63A4(UnkStruct_ov5_021D6594 **param0);
-BOOL ov5_021D6418(UnkStruct_ov5_021D6594 *param0, int param1, int param2);
-int ov5_021D64D0(UnkStruct_ov5_021D6594 *param0, int param1);
+void ov5_021D5F24(UnkStruct_ov5_021D5EF8 *param0, int weather);
+BOOL ov5_021D5F7C(UnkStruct_ov5_021D5EF8 *param0, int weather);
+u32 ov5_Get_unk_weather_04(UnkStruct_ov5_021D5EF8 *param0);
 
 #endif // POKEPLATINUM_OV5_021D5EB8_H
