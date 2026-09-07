3f799c5a16398832df84a9c2b6e66836ca553076
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2026-09-06 21:57:21 -0700

    Document Ribbons Save Data (#1285)

--- src/ribbon_save_data.c
+++ src/ribbon_save_data.c
@@ -1,18 +1,20 @@
-#include "unk_0202D778.h"
+#include "ribbon_save_data.h"
 
 #include <nitro.h>
 #include <string.h>
 
 #include "savedata.h"
 
+#define RIBBONS_SAVE_SIZE 14
+
 int Ribbons_SaveSize(void)
 {
-    return 14;
+    return RIBBONS_SAVE_SIZE;
 }
 
-void Ribbons_Init(u8 *param0)
+void Ribbons_Init(u8 *ribbons)
 {
-    memset(param0, 0, 14);
+    memset(ribbons, 0, RIBBONS_SAVE_SIZE);
 }
 
 u8 *SaveData_GetRibbons(SaveData *saveData)
